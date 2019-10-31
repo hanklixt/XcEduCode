8fb44696bbaf72d23543540bd04c3edc9a630593
hankli <1033110657@qq.com>
2019-10-31 16:48:47 +0800

    添加不在本程序中过的代码扫描

--- xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -2,11 +2,15 @@ package com.xuecheng.manage_cms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author lxt
  * @date 2019-10-31-16:18
  */
+@EntityScan("com.xuecheng.framework.domain.cms")//扫描实体类
+@ComponentScan(basePackages = {"com.xuecheng.api"})//扫描接口
 @SpringBootApplication
 public class ManageCmsApplication {
     public static void main(String[] args) {
