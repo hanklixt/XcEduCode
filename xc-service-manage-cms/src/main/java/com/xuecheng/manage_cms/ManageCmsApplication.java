d6dfb6291a4f1b6aef31e3640d07479fec259baa
hankli <1033110657@qq.com>
2019-10-31 16:28:57 +0800

    新建cms模块

---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/ManageCmsApplication.java
@@ -0,0 +1,15 @@
+package com.xuecheng.manage_cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author lxt
+ * @date 2019-10-31-16:18
+ */
+@SpringBootApplication
+public class ManageCmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageCmsApplication.class, args);
+    }
+}
